460f3816cfa7857bf899e126bfd5b378a42ae78a
syedmusaa <syedmusaali70@gmail.com>
2025-07-13 20:35:28 +0530

    go to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ['*'],
+    allowedHosts: ['.vercel.app'],
   },
   build: {
     outDir: "dist/spa",
